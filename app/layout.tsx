769d553be440dd951a8bf88d986cb0905f6c02fc
Vayu-create <sarthakdangwal3@gmail.com>
2025-06-17 16:42:58 +0530

    added GTM tag in layout.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'v0 App',
@@ -15,7 +16,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* ✅ Google Tag Manager - Head script */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KR5XRWX4');
+          `}
+        </Script>
+      </head>
       <body className={cn('min-h-screen bg-background font-sans antialiased')} suppressHydrationWarning>
+        {/* ✅ Google Tag Manager - NoScript iframe */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KR5XRWX4"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
